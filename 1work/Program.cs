6c68fcf65095c8f07313fb92d0f5187cae61a35a
Kretyg <sokolovsky.zhora58@gmail.com>
2023-02-05 16:32:28 +0300

    add 1 work

---
+++ 1work/Program.cs
@@ -0,0 +1,32 @@
+﻿// Показать двумерный массив размером m×n заполненный вещественными числами
+
+Console.Write("введите количество строк: ");
+int linesVol = int.Parse(Console.ReadLine());
+Console.Write("введите количество столбцов: ");
+int columnsVol = int.Parse(Console.ReadLine());
+double[,] numbers = new double[linesVol, columnsVol];
+FillArrayRandomNumbers(numbers);
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(1000)) / 100;
+        }
+    }
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
